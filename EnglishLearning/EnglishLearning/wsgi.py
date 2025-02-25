f2f39200816e990e4a97d91d32d75910162f5434
Jasmine Price <jevprice@googlemail.com>
2025-02-25 12:01:30 +0000

    update django settings in wsgi

--- EnglishLearning/EnglishLearning/wsgi.py
+++ EnglishLearning/EnglishLearning/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EnglishLearning.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EnglishLearning.EnglishLearning.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
